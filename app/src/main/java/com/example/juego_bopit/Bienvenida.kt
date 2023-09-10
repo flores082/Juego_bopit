b793ba03b7819bf6f13204d7aa01f9355f0f361a
Michel Flores <florespoblete17@gmail.com>
2023-09-10 17:31:28 -0300

    prueba inicio

---
+++ app/src/main/java/com/example/juego_bopit/Bienvenida.kt
@@ -0,0 +1,29 @@
+package com.example.juego_bopit
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+
+class Bienvenida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bienvenida)
+        VerBienvenida()
+    }
+
+    fun VerBienvenida(){
+        object : CountDownTimer(2000, 1000){
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
+                val intent = Intent(this@Bienvenida, MainActivity::class.java)
+                startActivity(intent)
+                finishAffinity()
+            }
+
+        }.start()
+    }
+}
