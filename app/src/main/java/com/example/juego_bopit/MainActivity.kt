941d86d474d7436b66f6c6c4dcf6a11ae22a6fac
Michel Flores <florespoblete17@gmail.com>
2023-09-13 22:56:53 -0300

    cambio de pantallas

--- app/src/main/java/com/example/juego_bopit/MainActivity.kt
+++ app/src/main/java/com/example/juego_bopit/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.juego_bopit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonOKAbout = findViewById<Button>(R.id.button)
+        val buttonOKAbout2 = findViewById<Button>(R.id.button2)
+
+        buttonOKAbout.setOnClickListener {
+            val intentAbout = Intent(this, About::class.java)
+            startActivity(intentAbout)
+        }
+
+        buttonOKAbout2.setOnClickListener {
+            val intentAbout2 = Intent(this, activity_preferencias::class.java)
+            startActivity(intentAbout2)
+        }
     }
 }
---
+++ app/src/main/java/com/example/juego_bopit/activity_preferencias.kt
@@ -0,0 +1,11 @@
+package com.example.juego_bopit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_preferencias : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preferencias)
+    }
+}
